49dc18af2a178b947891884533efa44ddedf697d
usamaking786 <usamaahmadansari@gmail.com>
2024-09-07 22:54:53 +0500

    React Redux Todo Project

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import AddTodo from './components/AddTodo'
+import Todos from './components/Todos.jsx'
+
+function App() {
+
+
+  return (
+    <div className='w-full'>
+    <AddTodo/>
+    <Todos/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { Provider } from 'react-redux'
+import App from './App.jsx'
+import { store } from './App/store.js'
+import './index.css'
+
+createRoot(document.getElementById('root')).render(
+  <Provider store={store}>
+    <App />
+    </Provider>
+)
